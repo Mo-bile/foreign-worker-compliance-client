9c260abfeba7d559e928ab32269353c02cea88bd
모재영 <mo@utransfer.com>
2026-03-23 12:20:02 +0900

    feat: extract ERROR_MESSAGES constants

---
+++ lib/constants/error-messages.ts
@@ -0,0 +1,6 @@
+export const ERROR_MESSAGES = {
+  SERVER_ERROR: "서버 오류가 발생했습니다",
+  INVALID_REQUEST_FORMAT: "잘못된 요청 형식입니다",
+  INVALID_INPUT: "입력값이 올바르지 않습니다",
+  PAGE_REFRESH_SUFFIX: "페이지를 새로고침해 주세요.",
+} as const;
